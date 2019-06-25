bf7e19a38dd6c80dc869270757219828d36b27d9
ramose <leopapilaya@gmail.com>
2019-06-25 13:18:23 +0800

    first commit

---
+++ android/app/src/main/java/com/jeemba/flutterchatapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.jeemba.flutterchatapp;
+
+import android.os.Bundle;
+
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
